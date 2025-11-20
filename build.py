8a1cc16a3a64a61047ce93806e51a75e351feeff
personnumber3377 <sontapaa.jokulainen@gmail.com>
2025-11-20 21:44:33 +0200

    Initial framework.

--- build.py
+++ build.py
@@ -80,13 +80,10 @@ def main():
 		# build_script = SCRIPTS_DIR / "fuzz"
 		# env["OUT_DIR"] = str(FUZZ_BIN_DIR)
 		# run_script(build_script, env)
-		cmd = """
-		source ./scripts/fuzz/common.sh
-		source ./scripts/fuzz/{}.sh
-		""".format(target) # Put the target thing...
+		cmd = """ bash -c 'source ./scripts/fuzz/common.sh ; source ./scripts/fuzz/{}.sh' """.format(target) # Put the target thing...
 		subprocess.run(cmd, shell=True)
-		
-		
+
+
 
 
 
--- scripts/fuzz/common.sh
+++ scripts/fuzz/common.sh
@@ -11,6 +11,8 @@ export CXX=clang++
 export CFLAGS="-fsanitize=address,undefined,fuzzer-no-link -g -O3"
 export CXXFLAGS="-fsanitize=address,undefined,fuzzer-no-link -g -O3"
 export LIB_FUZZING_ENGINE="-fsanitize=fuzzer" # Actually link the fuzzer here too...
-export OUT="$(realpath "$SCRIPT_DIR/../../work")"
+export OUT="$(realpath "$SCRIPT_DIR/../../fuzz_bin/")"
+export WORK="$(realpath "$SCRIPT_DIR/../../work/")"
+
 
 # echo $OUT
